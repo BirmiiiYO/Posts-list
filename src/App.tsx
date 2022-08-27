613ef413aa14833329f43b91ee1a94a5b585e79e
BirmiiiYo <avramenkoa773@gmail.com>
2022-08-27 22:06:03 +0300

    pagination, sort, styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header/Header';
+import Input from './components/Input/Input';
+import Pagin from './components/Pagination/Pagination';
+import Posts from './components/Posts/Posts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="content">
+          <Header />
+          <Input />
+          <main>
+            <Posts />
+          </main>
+          <Pagin />
+        </div>
+      </div>
     </div>
   );
 }
