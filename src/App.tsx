3ae0bd3fe0c7491c40d8d21a77a1200f28802956
BirmiiiYo <avramenkoa773@gmail.com>
2022-08-28 14:47:04 +0300

    problems w request

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CreatePostBlock from './components/CreatePostBlock/CreatePostBlock';
 import Header from './components/Header/Header';
 import Input from './components/Input/Input';
 import Pagin from './components/Pagination/Pagination';
@@ -5,18 +6,21 @@ import Posts from './components/Posts/Posts';
 
 function App() {
   return (
-    <div className="app">
-      <div className="container">
-        <div className="content">
-          <Header />
-          <Input />
-          <main>
-            <Posts />
-          </main>
-          <Pagin />
+    <>
+      <CreatePostBlock />
+      <div className="app">
+        <div className="container">
+          <div className="content">
+            <Header />
+            <Input />
+            <main>
+              <Posts />
+            </main>
+            <Pagin />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
